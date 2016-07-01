e9946a229b78a5802dd49552df0e690a1f5f7cc7
chengwenchao <chengwenchao@cd.tuan800.com>
2016-07-01 19:22:41 +0800

    no message

--- modules/main/src/main/java/com/main/NumberMain.java
+++ modules/main/src/main/java/com/main/NumberMain.java
@@ -6,7 +6,16 @@ public class NumberMain {
 	
 	public static void main(String args [])
 	{
+        //经典的带小数的值计算为非精准计算
+        double ad=2.3d;
+        System.out.println(new Double(ad*100).intValue());
 
+        //小数的精准计算
+        //不能传float或double作为BigDecimal构造参，只能用String作为构造参数
+        //如果用float或double作为BigDecimal构造参，multiply后计算的结果也是不精准的
+        BigDecimal vv=new BigDecimal("2.3");
+        BigDecimal rt =vv.multiply(new BigDecimal(100));
+        System.out.println(rt.intValue());
 
         double percent = 599 /(3600*1.0);
         BigDecimal bd = new BigDecimal("" + percent);
