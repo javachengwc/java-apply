03a79ec44a15afd1ee1041398413fe61686e2947
chengwenchao <chengwenchao@cd.tuan800.com>
2016-09-02 11:02:40 +0800

    no message

---
+++ modules/main/src/main/java/com/main/MapMain2.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain2 {
+
+    public static void main(String args [])
+    {
+        Map<Integer,Integer> aMap = new HashMap<Integer,Integer>();
+        Integer aa =aMap.get(null);
+    }
+}
