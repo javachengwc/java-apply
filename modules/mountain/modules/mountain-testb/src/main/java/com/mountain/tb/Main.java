f7435655de83ee4ee9981da7fe510576d66a9947
chengwenchao <chengwenchao@cd.tuan800.com>
2016-10-28 17:47:09 +0800

    no message

--- modules/mountain/modules/mountain-testb/src/main/java/com/mountain/tb/Main.java
+++ modules/mountain/modules/mountain-testb/src/main/java/com/mountain/tb/Main.java
@@ -1,6 +1,22 @@
 package com.mountain.tb;
 
+import com.mountain.tb.service.TransConsumeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String args [])
+    {
+        ClassPathXmlApplicationContext applicationContext =  new ClassPathXmlApplicationContext("/spring-alone.xml");
+        logger.info("Main testb start............");
+
+        TransConsumeService transConsumeService =applicationContext.getBean("transConsumeService",TransConsumeService.class);
+        transConsumeService.consume(1);
+
+    }
 
 }
---
+++ modules/mountain/modules/mountain-testb/src/main/java/com/mountain/tb/service/TransConsumeService.java
@@ -0,0 +1,39 @@
+package com.mountain.tb.service;
+
+import com.mountain.tb.finagle.trans.StringResult;
+import com.mountain.tb.finagle.trans.TransDealService;
+import com.util.ThreadUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 消费服务
+ */
+public class TransConsumeService {
+
+    private static Logger logger = LoggerFactory.getLogger(TransConsumeService.class);
+
+    private TransDealService.ServiceIface transService;
+
+    public void setTransService(TransDealService.ServiceIface transService) {
+        this.transService = transService;
+    }
+
+    public void consume(int dealId)
+    {
+        logger.info("TransConsumeService consume start");
+
+        ThreadUtil.sleep(2000l);
+        try {
+            StringResult result = transService.transDeal(dealId).toJavaFuture().get();
+            System.out.println("--------------transDeal result:\r\n");
+            System.out.println(result);
+
+        }catch(Exception e)
+        {
+            logger.info("TransConsumeService transDeal error,dealId="+dealId,e);
+        }
+        ThreadUtil.sleep(2000l);
+        logger.info("TransConsumeService consume end");
+    }
+}
