1e94e207bc244d1648cc1e27702c533ff5e9a75e
cheng <cheng@163.com>
2017-03-29 21:43:17 +0800

    no message

--- modules/main/src/main/java/com/arithmetic/find/ZheBanSearch.java
+++ modules/main/src/main/java/com/arithmetic/find/ZheBanSearch.java
@@ -10,9 +10,9 @@ public class ZheBanSearch {
 	public static int counter = 1; // 计数器
 
 	public static void main(String args[]) {
-		int KeyValue = 22;
+		int keyValue = 22;
 		// 调用折半查找
-		if (BinarySearch((int) KeyValue)) {
+		if (binarySearch(keyValue)) {
 			// 输出查找次数
 			System.out.println("");
 			System.out.println("Search Time = " + counter);
@@ -26,22 +26,22 @@ public class ZheBanSearch {
 	// ---------------------------------------------------
 	// 折半查找法
 	// ---------------------------------------------------
-	public static boolean BinarySearch(int KeyValue) {
+	public static boolean binarySearch(int keyValue) {
 		int left = 0; // 左边界变量
 		int right = data.length - 1; // 右边界变量
 		int middle; // 中位数变量
 
 		while (left <= right) {
 			middle = (left + right) / 2;
-			if (KeyValue < data[middle])
+			if (keyValue < data[middle])
 			{   // 欲查找值较小 查找前半段
 				right = middle - 1;
 			}
-			else if (KeyValue > data[middle])
+			else if (keyValue > data[middle])
 			{    // 欲查找值较大 查找后半段
 				left = middle + 1;
 			}
-			else if (KeyValue == data[middle]) 
+			else if (keyValue == data[middle])
 			{    // 查找到数据
 				System.out.println("data[" + middle + "] = " + data[middle]);
 				return true;
