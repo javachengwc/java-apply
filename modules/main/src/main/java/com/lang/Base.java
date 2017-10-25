877b0f10011d4bbf29902a4a9f58ed3e0bd94f4d
cheng <ccc@ccc.com>
2017-10-25 23:07:26 +0800

    no message

---
+++ modules/main/src/main/java/com/lang/Base.java
@@ -0,0 +1,11 @@
+package com.lang;
+
+public class Base {
+
+    protected  String var="BaseVar";
+
+    public void md() {
+        System.out.println("Base md invoked");
+    }
+
+}
---
+++ modules/main/src/main/java/com/lang/Sub.java
@@ -0,0 +1,11 @@
+package com.lang;
+
+public class Sub extends Base{
+
+    protected  String var="BaseVar";
+
+    public void md() {
+        System.out.println("Sub md invoked");
+    }
+
+}
