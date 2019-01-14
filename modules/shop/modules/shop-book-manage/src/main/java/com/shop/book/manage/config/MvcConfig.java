f4c10142d127f58e14e395d1d6aa8fd6af40175a
cheng <cheng@ccc.com>
2019-01-14 11:55:38 +0800

    no message

--- modules/shop/modules/shop-book-manage/src/main/java/com/shop/book/manage/config/MvcConfig.java
+++ modules/shop/modules/shop-book-manage/src/main/java/com/shop/book/manage/config/MvcConfig.java
@@ -2,12 +2,14 @@ package com.shop.book.manage.config;
 
 import com.shop.book.manage.filter.RequestReplacedFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
@@ -34,6 +36,16 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Bean(name = "requestReplacedFilter")
     public Filter requestReplacedFilter() {
         return new RequestReplacedFilter();
+    }
+
+
+    //设置上传文件大小,不设置就没有限制
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("500MB");
+        factory.setMaxRequestSize("500MB");
+        return factory.createMultipartConfig();
     }
 
 }
