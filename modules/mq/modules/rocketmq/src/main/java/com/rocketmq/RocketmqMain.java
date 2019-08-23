c52a72d73c7a6ceed9cc6a7df1d587a8f1eccc52
cheng <cheng@ccc.com>
2019-08-23 14:00:57 +0800

    no message

---
+++ modules/mq/modules/rocketmq/src/main/java/com/rocketmq/RocketmqMain.java
@@ -0,0 +1,10 @@
+package com.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+
+public class RocketmqMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqMain.class, args);
+    }
+}
---
+++ modules/mq/modules/rocketmq/src/main/java/com/rocketmq/config/Config.java
@@ -0,0 +1,33 @@
+package com.rocketmq.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class Config implements EnvironmentAware {
+
+    private static Logger logger = LoggerFactory.getLogger(Config.class);
+
+    private static String [] activeProfiles= null;
+
+    private static Environment environment;
+
+    @Override
+    public void setEnvironment(Environment env) {
+        environment=env;
+        activeProfiles = env.getActiveProfiles();
+        logger.info("----------------------Application profile={}--------------------", activeProfiles);
+    }
+
+    public static String [] getActiveProfiles() {
+        return activeProfiles;
+    }
+
+    public static Environment getEnvironment() {
+        return environment;
+    }
+
+}
