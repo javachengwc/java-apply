6f8c1f71ee0b93c8a32db99271c7b9378b081fda
cheng <ccc@163.com>
2020-02-21 09:46:17 +0800

    no message

--- modules/shop/modules/shop-order/order-api/src/main/java/com/shop/order/api/rest/OrderPayResource.java
+++ modules/shop/modules/shop-order/order-api/src/main/java/com/shop/order/api/rest/OrderPayResource.java
@@ -1,6 +1,5 @@
 package com.shop.order.api.rest;
 
-import com.shop.order.api.model.IcbcPayVo;
 import com.shop.order.api.model.OrderPayVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -22,14 +21,6 @@ public interface OrderPayResource {
     @RequestMapping(value = "/wxPayCreate", method = RequestMethod.POST)
     public OrderPayVo wxPayCreate(@RequestBody OrderPayVo orderPayVo, HttpServletRequest request);
 
-    @ApiOperation(value = "工行e支付下单", notes = "工行e支付下单")
-    @RequestMapping(value = "/icbcPayCreate", method = RequestMethod.POST)
-    public OrderPayVo icbcPayCreate(@RequestBody OrderPayVo orderPayVo, HttpServletRequest request);
-
-    @ApiOperation(value = "工行e支付支付", notes = "工行e支付支付")
-    @RequestMapping(value = "/icbcPay", method = RequestMethod.POST)
-    public IcbcPayVo icbcPay(@RequestBody IcbcPayVo icbcPayVo, HttpServletRequest request);
-
     @RequestMapping(value = "/aliPayCallback", method = RequestMethod.POST)
     @ApiOperation(value = "支付宝回调接口", notes = "支付宝回调接口")
     public String aliPayCallBack(HttpServletRequest request);
@@ -37,8 +28,4 @@ public interface OrderPayResource {
     @RequestMapping(value = "/wxPayCallback", method = RequestMethod.POST)
     @ApiOperation(value = "微信支付回调接口", notes = "微信支付回调接口")
     public String wxPayCallback(HttpServletRequest request);
-
-    @RequestMapping(value = "/icbcPayCallback", method = RequestMethod.POST)
-    @ApiOperation(value = "工行e支付回调接口", notes = "工行e支付回调接口")
-    public String icbcPayCallback(HttpServletRequest request);
 }
--- modules/shop/modules/shop-order/order-service/src/main/java/com/shop/order/controller/OrderPayController.java
+++ modules/shop/modules/shop-order/order-service/src/main/java/com/shop/order/controller/OrderPayController.java
@@ -1,14 +1,11 @@
 package com.shop.order.controller;
 
-import com.shop.order.api.model.IcbcPayVo;
 import com.shop.order.api.model.OrderPayVo;
 import com.shop.order.api.rest.OrderPayResource;
-import com.shop.order.service.OrderIcbcService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,9 +20,6 @@ public class OrderPayController implements OrderPayResource {
 
     private static Logger logger = LoggerFactory.getLogger(OrderPayController.class);
 
-    @Autowired
-    private OrderIcbcService orderIcbcService;
-
     @ApiOperation(value = "创建支付宝下单", notes = "创建支付宝下单")
     @RequestMapping(value = "/aliPayCreate", method = RequestMethod.POST)
     public OrderPayVo aliPayCreate(@RequestBody OrderPayVo orderPayVo, HttpServletRequest request) {
@@ -36,20 +30,6 @@ public class OrderPayController implements OrderPayResource {
     @RequestMapping(value = "/wxPayCreate", method = RequestMethod.POST)
     public OrderPayVo wxPayCreate(@RequestBody OrderPayVo orderPayVo, HttpServletRequest request) {
         return  null;
-    }
-
-    @ApiOperation(value = "工行e支付下单", notes = "工行e支付下单")
-    @RequestMapping(value = "/icbcPayCreate", method = RequestMethod.POST)
-    public OrderPayVo icbcPayCreate(@RequestBody OrderPayVo orderPayVo, HttpServletRequest request) {
-        return null;
-    }
-
-    @ApiOperation(value = "工行e支付支付", notes = "工行e支付支付")
-    @RequestMapping(value = "/icbcPay", method = RequestMethod.POST)
-    public IcbcPayVo icbcPay(@RequestBody IcbcPayVo icbcPayVo, HttpServletRequest request) {
-        logger.info("OrderPayController icbcPay start ,icbcPayVo={}",icbcPayVo);
-        IcbcPayVo payVo=orderIcbcService.icbcPay(icbcPayVo);
-        return payVo;
     }
 
     @RequestMapping(value = "/aliPayCallback", method = RequestMethod.POST)
@@ -64,9 +44,4 @@ public class OrderPayController implements OrderPayResource {
         return null;
     }
 
-    @RequestMapping(value = "/icbcPayCallback", method = RequestMethod.POST)
-    @ApiOperation(value = "工行e支付回调接口", notes = "工行e支付回调接口")
-    public String icbcPayCallback(HttpServletRequest request) {
-        return null;
-    }
 }
