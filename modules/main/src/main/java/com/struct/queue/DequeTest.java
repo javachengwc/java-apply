4fcd60672d68aed76c4abff7ef74caf51381dabd
cheng <cheng@ccc.com>
2021-08-21 00:14:17 +0800

    no message

--- modules/main/src/main/java/com/struct/queue/DequeTest.java
+++ modules/main/src/main/java/com/struct/queue/DequeTest.java
@@ -1,5 +1,30 @@
 package com.struct.queue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //Deque是双向队列，每一端都可以进行插入数据和删除数据操作。
 public class DequeTest {
+
+    public static void main(String [] args) {
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        deque.offer(1);
+        deque.offer(2);
+        deque.offer(3);
+        deque.offerFirst(0);
+        System.out.println("deque="+deque+",length="+deque.size());
+        Integer a=deque.pollLast();
+        System.out.println(a);
+        System.out.println("after pollLast,length="+deque.size());
+        deque.push(4);//放到first
+        deque.push(5);
+        deque.push(6);
+        System.out.println("deque="+deque+",length="+deque.size());
+        Integer b=deque.poll();
+        System.out.println(b);
+        Integer c = deque.pop();
+        System.out.println(c);
+        System.out.println("deque="+deque+",length="+deque.size());
+
+    }
 }
--- modules/main/src/main/java/com/struct/queue/QueueTest.java
+++ modules/main/src/main/java/com/struct/queue/QueueTest.java
@@ -1,5 +1,8 @@
 package com.struct.queue;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 //队列就是一个先入先出（FIFO）的数据结构
 //add        增加一个元索                     如果队列已满，则抛出一个IIIegaISlabEepeplian异常
 //remove     移除并返回队列头部的元素         如果队列为空，则抛出一个NoSuchElementException异常
@@ -14,6 +17,17 @@ package com.struct.queue;
 public class QueueTest {
 
     public static void main(String [] args ) {
-
+        Queue<Integer> queue = new ArrayBlockingQueue<Integer>(3);
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        System.out.println("queue="+queue+",length="+queue.size());
+        Integer a =queue.poll();
+        System.out.println(a);
+        System.out.println("after poll,queue="+queue+",length="+queue.size());
+        Integer b =queue.peek();
+        System.out.println(b);
+        System.out.println("after peek,queue="+queue+",length="+queue.size());
     }
 }
