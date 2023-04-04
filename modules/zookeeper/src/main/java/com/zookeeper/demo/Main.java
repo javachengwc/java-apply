55737d5bb382e88f61f020cf3b02741237c4ea28
chengwc2 <chengwc2@ccc.com>
2023-04-04 10:32:37 +0800

    no message

--- modules/zookeeper/src/main/java/com/zookeeper/demo/Main.java
+++ modules/zookeeper/src/main/java/com/zookeeper/demo/Main.java
@@ -1,17 +1,19 @@
 package com.zookeeper.demo;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 
 public class Main {
 	
 	public static void main(String args []) throws Exception 
 	{
-		ZooKeeper zk=new ZooKeeper("127.0.0.1:5000", 5000, new Watcher() {
+		ZooKeeper zk=new ZooKeeper("xxx:2181", 5000, new Watcher() {
             
 			CountDownLatch down=new CountDownLatch(1);//同步阻塞状态  
             @Override  
@@ -24,7 +26,20 @@ public class Main {
             }  
         });  
           
-        System.out.println("连接成功："+zk.toString());  
+        System.out.println("连接成功："+zk.toString());
+		Stat stat = zk.exists("/dubbo/com.xxx.XxxService",false);
+		System.out.println(stat!=null);
+		Stat stat2 = zk.exists("/dubbo/com.xxx.XxxService/consumers",false);
+		System.out.println(stat2!=null);
+
+		List<String> list  = zk.getChildren("/dubbo/com.xxx.XxxService/providers",false);
+		int cnt = list ==null ? 0: list.size();
+		System.out.println("list.size="+cnt);
+		if(cnt>0) {
+			for(String per: list) {
+				System.out.println(per);
+			}
+		}
         zk.close();//关闭连接  
 	}
 
