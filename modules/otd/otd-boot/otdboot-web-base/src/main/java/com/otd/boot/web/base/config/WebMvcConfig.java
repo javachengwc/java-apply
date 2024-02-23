cfb4ddc3010e01d48523b6995bc8ca5cea34fb70
chengwc2 <chengwc2@ccc.com>
2024-02-23 17:21:34 +0800

    no message

--- modules/otd/otd-boot/otdboot-web-base/src/main/java/com/otd/boot/web/base/config/WebMvcConfig.java
+++ modules/otd/otd-boot/otdboot-web-base/src/main/java/com/otd/boot/web/base/config/WebMvcConfig.java
@@ -1,13 +1,17 @@
 package com.otd.boot.web.base.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  * web配置
@@ -19,14 +23,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Bean
   public CorsFilter corsFilter() {
     CorsConfiguration config = new CorsConfiguration();
-    config.addAllowedOrigin("*");
     config.setAllowCredentials(true);
     config.addAllowedMethod("*");
     config.addAllowedHeader("*");
+    config.setAllowedOriginPatterns(Lists.newArrayList("*"));
     UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
     configSource.registerCorsConfiguration("/**", config);
     return new CorsFilter(configSource);
   }
+
+//  跨域或者
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")             // 允许跨域访问的路径
+//            .allowedOriginPatterns("*")    // 允许跨域访问的源
+//  }
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -41,7 +52,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void configurePathMatch(PathMatchConfigurer configurer) {
     // 支持 .do 后缀接口
     configurer.setUseTrailingSlashMatch(true).setUseSuffixPatternMatch(true);
-    WebMvcConfigurer.super.configurePathMatch(configurer);
+
+//    UrlPathHelper urlPathHelper = new UrlPathHelper();
+//    configurer.setUrlPathHelper(urlPathHelper);
+    // 这里设置没启作用，还是需要在  application.properties/yml文件中设置
+    // spring.mvc.pathmatch.matching-strategy=ant_path_matcher才生效
+    AntPathMatcher antPathMatcher =new AntPathMatcher();
+    configurer.setPathMatcher(antPathMatcher);
+
   }
 
 }
