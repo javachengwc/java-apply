2f086bbd4c101149a796e0cae7df1b07ca6cd627
cheng <cheng@qq.com>
2025-03-04 00:12:37 +0800

    no message

--- modules/commonservice/modules/commonservice-manage-vue3/src/views/login.vue
+++ modules/commonservice/modules/commonservice-manage-vue3/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
+      <h3 class="title">通用管理系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -59,7 +59,6 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-2025 ruoyi.vip All Rights Reserved.</span>
     </div>
   </div>
 </template>
